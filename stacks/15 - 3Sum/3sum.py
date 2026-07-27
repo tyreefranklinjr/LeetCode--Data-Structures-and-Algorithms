4b58aed618f80705a615c1ba692694be11fc27c0
Tyree Franklin Jr. <tyree.franklinjr@gmail.com>
2026-07-27 15:19:18 -0500

    Add threeSum function to solve 3Sum problem
    Implement threeSum function to find triplets that sum to zero.

---
+++ stacks/15 - 3Sum/3sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+
+        res = []; nums.sort()
+        
+        for i, a, in enumerate(nums):
+            if i > 0 and a == nums[i - 1]: continue
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0: r -= 1
+                elif threeSum < 0: l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r: l += 1
+
+        return res           
+
+        # Time Complexity -> O(N^2) as N is the number of elements in nums
+        # Space Complexity -> O(1) as the auxillary space remains consistant
